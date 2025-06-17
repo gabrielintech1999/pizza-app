5c82e5f0200a662304bb55e8e8e88f4835cac083
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2025-06-17 14:41:18 +0100

    instalation and setup of enviroment...

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function infoName(name) {
+    console.log(name);
+}
+infoName("Gabriel");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function infoName(name: string) {
+  console.log(name);
+}
+
+infoName("Gabriel");
